a0eec067f3fc98c25e94b1d8be614f089ef01ad3
huangxueliang <huangxueliang@lbdfun.com>
2020-05-06 11:34:48 +0800

    svg图标

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,30 @@
 <template>
   <div id="app">
-    <div id=nav>
-      <router-link to="/">Home</router-link> | 
-      <router-link to="/about">About</router-link> | 
-      <router-link to="/login">Login</router-link> | 
+    <div id="nav">
+      <router-link to="/">
+        <svg-icon icon-class="home"></svg-icon>Home
+      </router-link>|
+      <router-link to="/about">
+        <svg-icon icon-class="about"></svg-icon>About
+      </router-link>|
+      <router-link to="/login">
+        <svg-icon icon-class="login"></svg-icon>Login
+      </router-link>|
       <span>{{loginState}}</span>
     </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
 <script>
-import {mapState,mapGetters} from 'vuex'
+import { mapState, mapGetters } from "vuex";
 export default {
   computed: {
-    ...mapState(['isLogin']),
-    ...mapGetters(['loginState'])
+    ...mapState(["isLogin"]),
+    ...mapGetters(["loginState"])
   }
-}
+};
 </script>
 
 <style lang="less">
-
 </style>
--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,46 @@
 // node 代码
+const path = require('path');
+function resolve(dir) {
+    console.log(dir);
+    return path.join(__dirname, dir)
+}
+
+const port = 7070;
+const title = 'vue项目最佳实践';
+
 module.exports = {
     publicPath: '/kcart',
     configureWebpack: {
         devServer: {
+            port, // 修改端口7070
             before(app) {
-                app.get('/goods',(req,res)=>{
+                app.get('/goods', (req, res) => {
                     res.json([
-                        {id:1, text: 'abc1'},
-                        {id:2, text: 'abc2'},
+                        { id: 1, text: 'abc1' },
+                        { id: 2, text: 'abc2' },
                     ])
                 })
             }
         }
+    },
+    configureWebpack: {
+        name: title // 向index.html注入标题
+    },
+    chainWebpack(config) {
+        // 改svg默认规则：排除icons目录中svg文件处理
+        config.module
+            .rule('svg')
+            .exclude.add(resolve('src/icons'))
+            .end();
+        // 新增icons规则，设置svg-sprite-loader处理icons目录中的svg
+        config.module
+            .rule('icons')
+            .test(/\.svg$/)
+            .include.add(resolve('src/icons'))
+            .end()
+            .use('svg-sprite-loader')
+            .loader('svg-sprite-loader')
+            .options({symbolId: 'icon-[name]'})
+            .end();
     }
 }
